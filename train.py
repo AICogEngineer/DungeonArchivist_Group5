dfae9fe37df25fd9fbdaa6f37d2a6e8c1f1938b1
Koshik Mahapatra <kmahapatra@Koshiks-MacBook-Pro.local>
2025-12-23 14:30:10 -0500

    Added persistent ChromaDB setup in init

--- train.py
+++ train.py
@@ -4,3 +4,20 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
+import chromadb
+
+def __init__(self, artifacts_dir, chroma_path, collection_name):
+    self.artifacts_dir = artifacts_dir
+    self.chroma_path = chroma_path
+    self.collection_name = collection_name
+
+    self.client = chromadb.PersistentClient(path=self.chroma_path)
+    self.collection = self.client.get_or_create_collection(
+    name=self.collection_name,
+        metadata={"hnsw:space": "cosine"},
+    )
+
+
+
+
+
